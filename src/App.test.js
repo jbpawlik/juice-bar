58b7f2f9bc45141483bb95d444de309fe5a669fa
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-09-10 09:12:37 -0700

    add tests to render homepage; passing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,37 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './components/App';
+import ShallowRenderer from 'react-test-renderer/shallow';
+import Header from './components/Header';
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { shallow } from "enzyme";
+import React from 'react';
+import '@testing-library/jest-dom'
 
-test('renders learn react link', () => {
+
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const renderer = new ShallowRenderer();
+renderer.render(<App />);
+const result = renderer.getRenderOutput();
+
+expect(result.props.children).toEqual(
+  <Header />
+);
+
+test('renders homepage', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Juice/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+it("renders without crashing", () => {
+  shallow(<App />);
+});
+
+it("renders Account header", () => {
+  const wrapper = shallow(<App />);
+  const welcome = <Header/>;
+  expect(wrapper.contains(welcome)).toEqual(true);
 });
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <React.Fragment>
+      <h1>Hummingbird Juice Bar</h1>
+    </React.Fragment>
+  )
+}
+
+
+export default Header;
